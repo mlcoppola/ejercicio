f937d5cc705c90adf79d012638ba672650c2e52e
Maximiliano Cóppola <maxicopp@gmail.com>
2020-10-07 03:16:59 -0300

    Se agrega footer

--- src/components/landing/Footer.js
+++ src/components/landing/Footer.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 export const Footer = () => {
     return (
-        <footer className="container">
+        <footer className="footer__container">
             <p>
-                <h4>Gracias por completar el ejercicio</h4>
-                <p>Te invitamos a ver más información</p>
+                <h4 className="footer__agradecimiento">Gracias por <span>completar el ejercicio</span></h4>
+                <p className="footer__mas-info">Te invitamos a ver más información</p>
             </p>
 
-            <button>Conocer más</button>
+            <button className="footer__button-conocer">Conocer más</button>
+
+            <p>WOLOX</p>
         </footer>
     )
 }
