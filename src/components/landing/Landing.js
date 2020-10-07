b13db5e31e4ba8638300cc4370c66b27311b0b07
Maximiliano Cóppola <maxicopp@gmail.com>
2020-10-07 03:37:36 -0300

    Animación

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -5,10 +5,10 @@ import { Main } from './Main';
 
 export const Landing = () => {
     return (
-        <>
+        <div className="animate__animated animate__fadeIn">
             <Navbar />
             <Main />
             <Footer />
-        </>
+        </div>
     )
 }
